d2d3e3c5948932e9b3ea0466cef73b841c066392
josegarcia1519 <josegarcia1519@gmail.com>
2021-10-15 11:30:47 -0600

    lab5

---
+++ server/index.js
@@ -0,0 +1,38 @@
+const express = require("express"); 
+const cors = require("cors");
+
+const app = express();
+
+
+app.use(cors());
+
+app.use(express.json()); // When we want to be able to accept JSON.
+
+app.get("/api/compliment", (req, res) => {
+  const compliments = ["Gee, you're a smart cookie!",
+					 "Cool shirt!",
+					 "Your Javascript skills are stellar.",
+  ];
+};
+app.get("/api/fortune", (req, res) => {
+    const fortunes = ["A beautiful, smart, and loving person will be coming into your life",
+    "A dubious friend may be an enemy in camouflage",
+    "A faithful friend is a strong defense",
+];
+};
+  //chose random fortune
+ let randomId = Math.floor(Math.random() * fortunes.length);
+ let randomFortune = fortunes[randomId];
+ let random
+ res.status(200).send(randomFortune);
+
+  // choose random compliment
+  let randomIndex = Math.floor(Math.random() * compliments.length);
+  let randomCompliment = compliments[randomIndex];
+  // choose random cookie
+  let random
+
+  res.status(200).send(randomCompliment);
+  
+
+app.listen(4000, () => console.log("Server running on 4000"));
